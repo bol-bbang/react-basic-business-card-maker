138e37c665cf8558af5f3345245060d416e4bbb4
bol-bbang <hh.park@kakao.com>
2021-05-31 23:39:15 +0900

    login with firebase authentication

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../header/header.module.css';
+
+const Footer = (props) => (
+      <footer className={styles.header}>
+        <h2>Code your dream</h2>
+      </footer>
+  );
+
+export default Footer;
---
+++ src/components/login/login.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+import styles from './login.module.css';
+import loginAuth from '../../service/loginAuth.js';
+
+const Login = (props) => {
+
+  const onClick = (e) => {
+
+    handleAuth(e.target)
+    .then(result => {
+      console.log(result);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+    
+  }
+  const handleAuth = (target) => {
+    const auth = new loginAuth();
+    switch(target.innerText){
+      case 'Google': return auth.google();
+      case 'Github': return auth.github();
+    }
+    
+  }
+
+  return (
+    <div className={styles.container}>
+      <Header />
+      <div className={styles.buttons}>
+        <h3>Login</h3>
+        <button onClick={onClick}>Google</button>
+        <button onClick={onClick}>Github</button>
+      </div>
+      <Footer />
+    </div>
+    );
+}
+
+export default Login;
