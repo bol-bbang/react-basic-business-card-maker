0eaf0a997ac748b4111ebef7abdb195e3063f0bd
bol-bbang <hh.park@kakao.com>
2021-06-02 23:57:13 +0900

    add router

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,53 @@
 import styles from './app.module.css';
 import Login from './components/login/login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className={styles.shadow}>
-      <Login />
-    </div>
+    
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <div className={styles.shadow}>
+            <Login />
+          </div>
+        </Route>
+        <PrivateRoute path="/protected">
+          <ProtectedPage />
+        </PrivateRoute>
+      </Switch>
+    </Router>
+
+);
+}
+function ProtectedPage() {
+  return <h3>Protected ~ user!!! log-in!!!!!!</h3>;
+}
+
+function PrivateRoute({ children, ...rest }) {
+  // let auth = useAuth();
+  let auth = { user : 'bol-bbang' };
+  return (
+    <Route
+      {...rest}
+      render = {({ location }) =>
+        auth.user ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
   );
 }
 
