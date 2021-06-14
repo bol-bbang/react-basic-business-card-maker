1c41e8b64c41c4423da48074da225aeade8162fd
bol-bbang <hh.park@kakao.com>
2021-06-14 23:58:46 +0900

    add cardEditForm layout

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import styles from './card_edit_form.module.css';
 
-const CardEditForm = ({ card }) => (
+const CardEditForm = ({ card }) => {
+  const { name, company, title, phone, email, theme, message, fileName, fileURL } = card;
+
+  return(
       <form>
-        <ul>
-          <li>
-            <span>name</span>
-            <input type="text" name="name" />
-          </li>
-        </ul>
+        <input type="text" name="name" value={name} />
+        <input type="text" name="company" value={company} />
+        <input type="text" name="title" value={title} />
+        <input type="text" name="phone" value={phone} />
+        <input type="text" name="email" value={email} />
+        <select name="theme" value={theme}>
+          <option value="dark">dark</option>
+          <option value="light">light</option>
+          <option value="colorful">colorful</option>
+        </select>
+        <textarea name="message" value={message}></textarea>
+        <input type="text" name="fileName" value={fileName} />
       </form>
   );
+}
 
 export default CardEditForm;
