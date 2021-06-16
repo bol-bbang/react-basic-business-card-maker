d233e5c08f57b2e50ea11aa1bf0083148f7fe2e3
bol-bbang <hh.park@kakao.com>
2021-06-16 23:58:48 +0900

    style cardEditForm 2nd

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -5,23 +5,26 @@ import styles from './card_edit_form.module.css';
 
 const CardEditForm = ({ card }) => {
   const { name, company, title, phone, email, theme, message, fileName, fileURL } = card;
-  const onsubmit = () => {}
+  const onSubmit = () => {}
+  const onChange = () => {}
 
   return(
       <form className={styles.form}>
-        <input className={styles.input} type="text" name="name" value={name} />
-        <select className={styles.select} name="theme" value={theme}>
+        <input className={styles.input} type="text" name="name" value={name} onChange={onChange} />
+        <select className={styles.select} name="theme" value={theme} onChange={onChange}>
           <option value="dark">dark</option>
           <option value="light">light</option>
           <option value="colorful">colorful</option>
         </select>
-        <input className={styles.input} type="text" name="company" value={company} />
-        <input className={styles.input} type="text" name="title" value={title} />
-        <input className={styles.input} type="text" name="phone" value={phone} />
-        <input className={styles.input} type="text" name="email" value={email} />
-        <textarea className={styles.textarea} name="message" value={message}></textarea>
-        <ImageFileInput />
-        <Button name="Delete" onClick={onsubmit}/>
+        <input className={styles.input} type="text" name="company" value={company} onChange={onChange} />
+        <input className={styles.input} type="text" name="title" value={title} onChange={onChange} />
+        <input className={styles.input} type="text" name="phone" value={phone} onChange={onChange} />
+        <input className={styles.input} type="text" name="email" value={email} onChange={onChange} />
+        <textarea className={styles.textarea} name="message" value={message} onChange={onChange}></textarea>
+        <div className={styles.fileInput}>
+          <ImageFileInput fileName={fileName} />
+        </div>
+        <Button name="Delete" onClick={onSubmit}/>
       </form>
   );
 }
