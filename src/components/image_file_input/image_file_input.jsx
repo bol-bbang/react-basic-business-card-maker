e20d2bd81a99efdeb7da674602f11773605c5417
bol-bbang <hh.park@kakao.com>
2021-06-21 23:51:40 +0900

    attach input file in image_file_input.jsx

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import styles from './image_file_input.module.css';
 
 const ImageFileInput = (props) => (
-  <button className={styles.button}>Image</button>
+  <>
+    <button className={styles.button}>Image</button>
+    <input name="file" type="file"
+    class="file-upload" data-cloudinary-field="image_id"
+    data-form-data="{ 'transformation': {'crop':'limit','tags':'samples','width':3000,'height':2000}}"/>
+  </>
   );
 
 export default ImageFileInput;
