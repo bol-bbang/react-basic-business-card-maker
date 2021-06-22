f309e8b36d1815e11434cb229d4a1839ddd78802
bol-bbang <hh.park@kakao.com>
2021-06-22 23:58:06 +0900

    add component props

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.module.css';
 import App from './app';
 import AuthService from './service/auth_service';
+import UploadService from './service/upload_service';
+import ImageFileInput from './components/image_file_input/image_file_input';
 
 const authService = new AuthService();
+const uploadService = new UploadService();
+const FileInput = props => {
+  <ImageFileInput {...props} uploadService={uploadService}/>
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App authService={authService}/>
+    <App FileInput={FileInput} authService={authService} />
   </React.StrictMode>,
   document.getElementById('root')
 );
